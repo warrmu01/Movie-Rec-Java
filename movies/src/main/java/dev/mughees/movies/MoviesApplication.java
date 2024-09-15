c098ab0c95974fe8793c4b45fa3685a8dff569e4
Mughees Warraich <warrmu01@luther.edu>
2024-09-15 17:47:04 -0500

    Movie.java completed

--- movies/src/main/java/dev/mughees/movies/MoviesApplication.java
+++ movies/src/main/java/dev/mughees/movies/MoviesApplication.java
@@ -2,6 +2,8 @@ package dev.mughees.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -9,5 +11,6 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+
 
 }
